e4142b9ffce4c2f1cf27a6dcf570cda2a2e130ae
Spooro <52739706+Spooro@users.noreply.github.com>
2025-01-19 22:57:08 -0500

    Add complete worker code

---
+++ workers/withered-lab-04de/src/index.ts
@@ -0,0 +1,59 @@
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+
+type Bindings ={
+	OPEN_AI_KEY: string;
+	AI: Ai;
+}
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use('/*', cors(
+	{
+		origin: '*',
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	}
+));
+
+app.post('/translateDocument', async (c) => {
+	const {documentData, targetLang} = await c.req.json();
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn',{
+		input_text: documentData,
+		max_length: 1000,
+	});
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b',{
+	text: summaryResponse.summary,
+	source_lang: 'english',
+	target_lang: targetLang,
+});
+
+	return new Response(JSON.stringify(response));
+});
+
+app.post('/chatToDocument', async (c) => {
+const  openai = new OpenAI({
+	apiKey: c.env.OPEN_AI_KEY,
+});
+const {documentData, question} = await c.req.json();
+const chatCompletion = await openai.chat.completions.create({
+	messages: [
+		{role: 'system', content: `You are a helpful assistant helping the user to chat to a document, I am providing a JSON file of the markdown for the document. Using this, answer the users questions in the clearest way possible, the document is about ` + documentData},
+		{role: 'user', content: "My Question is: " + question},
+	],
+	model: 'gpt-3.5-turbo',
+	temperature: 0.5,
+});
+const response = chatCompletion.choices[0].message.content;
+return c.json({message:response});
+});
+
+app.get('/', (c) => {
+    return c.text('Server is running');
+});
+
+export default app;
